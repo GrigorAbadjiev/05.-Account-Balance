f6343839162a503c093473b3ab2587b6b1b98753
User <User@DESKTOP-ESP2JT1>
2022-11-30 16:50:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double totalSum = 0;
+
+            while (input != "NoMoreMoney")
+            {
+                double inputAsNumber = double.Parse(input);
+                if (inputAsNumber < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {inputAsNumber:f2}");
+                totalSum += inputAsNumber;
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {totalSum:f2}");
+        }
+    }
+}
